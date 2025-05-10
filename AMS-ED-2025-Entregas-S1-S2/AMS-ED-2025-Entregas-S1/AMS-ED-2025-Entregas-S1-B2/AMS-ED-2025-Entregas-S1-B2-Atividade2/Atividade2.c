49dea8cf0ff907763a5141cefd8a0b51619734ab
Vinícius da Silva Ramos <Vinicius.ramos35@fatec.sp.gov.br>
2025-05-10 09:36:31 -0300

    Entrega S1-B2-2

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/Atividade2.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/Atividade2.c
@@ -13,15 +13,38 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX 5
+
+void bubbleSort(float *vetor, int tamanho) {
+    float temp;
+    for (int x = 0; x < tamanho - 1; x++) {
+        for (int j = 0; j < tamanho - 1 - x; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void exibir(float *vetor, int tamanho) {
+    printf("Array ordenado em ordem ascendente:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%.2f ", vetor[i]);
+    }
+    printf("\n");
+}
+
 int main() {
     float *vetor;
-    int i = 0, j;
-    float entrada, temp;
+    int i = 0;
+    float entrada;
     char buffer[100];
     char extra;
-    char resposta;
+    char resposta[10];
 
-    vetor = (float*) malloc(5 * sizeof(float));  
+    vetor = (float*) malloc(MAX * sizeof(float));  
     if (vetor == NULL) {
         printf("Erro na alocação de memória!\n");
         return 1;
@@ -35,7 +58,6 @@ int main() {
             continue;
         }
 
-        
         if (sscanf(buffer, "%f %c", &entrada, &extra) != 1) {
             printf("Entrada inválida! Por favor, digite um número válido.\n");
             continue;
@@ -44,35 +66,22 @@ int main() {
         vetor[i] = entrada;
         i++;
 
-        if (i >= 5) {
-            printf("Limite de 5 números atingido.\n");
+        if (i >= MAX) {
+            printf("Limite de %d números atingido.\n", MAX);
             break;
         }
 
         printf("Deseja digitar outro número? (s/n): ");
-        resposta = getchar();
-
-    
-        while (getchar() != '\n');
-
-    } while (resposta == 's' || resposta == 'S');
-
-    
-    for (int x = 0; x < i - 1; x++) {
-        for (j = 0; j < i - 1 - x; j++) {
-            if (vetor[j] > vetor[j + 1]) {
-                temp = vetor[j];
-                vetor[j] = vetor[j + 1];
-                vetor[j + 1] = temp;
-            }
+        if (fgets(resposta, sizeof(resposta), stdin) != NULL) {
+            resposta[0] = tolower(resposta[0]);
+        } else {
+            resposta[0] = 'n';
         }
-    }
 
-    printf("Array ordenado em ordem ascendente:\n");
-    for (j = 0; j < i; j++) {
-        printf("%.2f ", vetor[j]);
-    }
-    printf("\n");
+    } while (resposta[0] == 's');
+
+    bubbleSort(vetor, i);
+    exibir(vetor, i);
 
     free(vetor);
     return 0;
